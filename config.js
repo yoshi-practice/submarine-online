c0794d1f15c443b580ce8913cabfbf1bf4accd8f
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-03-27 11:33:49 +0900

    [Update] 雛形を追加

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  twitter: {
+    consumerKey: '',
+    consumerSecret: '',
+    callbackURL: 'http://localhost:8000/oauth_callback'
+  },
+  ipAddress: 'http://localhost:8000'
+};
